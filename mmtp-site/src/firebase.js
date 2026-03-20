bfe11a448002e7da94a834aa2b717c43df0c31c4
prodaaron <aaronmesfin60@gmail.com>
2026-03-20 14:32:03 +0800

    initialization

---
+++ mmtp-site/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCzOq4NZzCKJ_QjuRRLdqB62-9rLNHrpiY",
+  authDomain: "mmtp-mehha.firebaseapp.com",
+  projectId: "mmtp-mehha",
+  storageBucket: "mmtp-mehha.firebasestorage.app",
+  messagingSenderId: "225946232316",
+  appId: "1:225946232316:web:481ae43fe155f4d93dfe79",
+  measurementId: "G-4VRCKME5SX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
