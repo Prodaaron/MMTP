50ba8d6454fec1e6be0733922d0f4cc2a5fcd39e
prodaaron <aaronmesfin60@gmail.com>
2026-03-28 17:29:17 +0800

    Untracked changes

--- mmtp-site/src/components/About.jsx
+++ mmtp-site/src/components/About.jsx
@@ -30,12 +30,16 @@ export default function About() {
           viewport={{ once: true }}
         >
           <motion.h2 className="title" variants={item}>
-            What is MMTP?
+            <h2 className="sec-header">What is MMTP?</h2>
           </motion.h2>
 
           <motion.p className="subtitle" variants={item}>
-            MMTP (Modern Market Trade Program) is a structured initiative designed to
-            train individuals in international trade, market research, and real-world execution.
+            MMTP (MEHHA Marketing & Trade Program) is a structured initiative designed to
+            involve individuals in international trade, market research, and real-world execution.
+            Participants gain hands-on experience in navigating global markets, conducting in-depth
+            research, and implementing strategies that drive business growth. This program bridges
+            the gap between theoretical knowledge and practical application, empowering users to
+            engage in meaningful trade activities and contribute to international commerce.
           </motion.p>
 
           <motion.div className="grid-3" variants={container}>
